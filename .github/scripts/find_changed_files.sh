e65486fe1b8dfe7fb7bc6e68c3e01a51d4c2b428
James T. Kirk <75287890+esureos@users.noreply.github.com>
2024-04-22 00:43:21 +0800

    Update find_changed_files.sh

--- .github/scripts/find_changed_files.sh
+++ .github/scripts/find_changed_files.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
 # 指定要检查的文件列表
-FILES_TO_CHECK=("config.env" "config.gki.env" "config.rubens.env" "config.ginkgo.env" "config.gki_ksu.env" "config.rubens_ksu.env" "config.rubens_ksu.env" "config.gki_lxcd.env" "config.rubens_lxcd.env" "config.ginkgo_lxcd.env" "config.gki_ksulxcd.env" "config.rubens_ksulxcd.env" "config.ginkgo_ksulxcd.env")
+FILES_TO_CHECK=("file1" "file2" "file3")
 
 # 获取最近一次提交中所有更改的文件列表
 CHANGED_FILES=$(git diff --name-only HEAD~1 HEAD)
 
-# 初始化变量来存储更改的文件
-CHANGED_FILE=""
-
-# 检查指定的文件是否有更改
+# 遍历指定的文件列表
 for file in "${FILES_TO_CHECK[@]}"; do
+    # 检查当前文件是否在更改的文件列表中
     if echo "$CHANGED_FILES" | grep -q "$file"; then
-        # 如果已经找到一个更改的文件，则退出循环
-        if [ -n "$CHANGED_FILE" ]; then
-            echo "列表中有多个文件更改。"
-            exit 1
-        fi
-        CHANGED_FILE=$file
+        echo "文件 '$file' 有更改。"
+        # 将有更改的文件赋值给 GITHUB_ENV
+        GITHUB_ENV=$file
+        # 这里可以添加您想要执行的命令，例如：
+        echo "当前 GITHUB_ENV: $GITHUB_ENV"
+        # 如果需要在这里执行命令，可以取消下一行的注释
+        # some_command_to_run
     fi
 done
-
-# 如果列表中只有一个文件更改，则将其赋值给 GITHUB_ENV
-if [ -n "$CHANGED_FILE" ]; then
-    echo "文件 '$CHANGED_FILE' 是唯一更改的文件。"
-    GITHUB_ENV=$CHANGED_FILE
-else
-    echo "没有文件更改。"
-fi
-
-# 输出 GITHUB_ENV 的值
-echo "GITHUB_ENV: $GITHUB_ENV"
